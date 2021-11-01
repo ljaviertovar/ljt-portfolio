606adea6ddc8e01405fd4812ee2de116f5b6657f
ljaviertovar <luisjavier_tovar@outlook.com>
2021-11-01 17:52:20 -0600

    :sparkles: [ADD] Services component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const Home = () => {
     return (
         <>
             <HeroSection/>
             <AboutSection/>
+            <ServicesSection />
         </>
     )
 }
