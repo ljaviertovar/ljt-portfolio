9526afdd849cdbf7019a2f926b12c80489e648d4
ljaviertovar <luisjavier_tovar@outlook.com>
2021-11-12 09:23:03 -0600

    :sparkles: [ADD] Projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import SectionTitle from '../components/SectionTitle'
+import ProjectItem from '../components/ProjectItem'
+
+import ProjectInfo from '../data/projects'
+
+const ProjectsStyles = styled.div`
+    padding: 10rem 0;
+    .project__allItems {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 5rem;
+        margin-top: 5rem;
+    }
+`
 
 const Projects = () => {
+
+    const [projectData, setProjectData] = useState(ProjectInfo)
+
     return (
-        <div>
-            <h1>Project page</h1>
-            
-        </div>
+        <ProjectsStyles>
+            <div className='container'>
+                <SectionTitle
+                    heading='Projects'
+                    subheading='some of my recent works'
+                />
+                <div className='project__allItems'>
+                    {
+                        projectData.map((project) => {
+                            return (
+                                <ProjectItem
+                                    key={project.id}
+                                    title={project.name}
+                                    desc={project.desc}
+                                    img={project.img}
+                                />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+        </ProjectsStyles>
     )
 }
 
