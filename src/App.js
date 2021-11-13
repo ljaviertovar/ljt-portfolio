2e7c0a6c48975cf559ed914301b888b19e15fb5c
ljaviertovar <luisjavier_tovar@outlook.com>
2021-11-12 19:50:23 -0600

    :sparkles: [ADD] Scroll component

--- src/App.js
+++ src/App.js
@@ -7,26 +7,29 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
+import SmoothScrollbar from './components/SmoothScrollbar';
 
 export default function App() {
 	return (
 		<Router>
 			<NavMenu />
-			<Switch>
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route path="/projects">
-					<Projects />
-				</Route>
-				<Route path="/contact">
-					<Contact />
-				</Route>
-				<Route path="/">
-					<Home />
-				</Route>
-			</Switch>
-            <Footer/>
+			<SmoothScrollbar>
+				<Switch>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route path="/projects">
+						<Projects />
+					</Route>
+					<Route path="/contact">
+						<Contact />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+				<Footer />
+			</SmoothScrollbar>
 		</Router>
 	);
 }
---
+++ src/components/SmoothScrollbar.js
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from 'react'
+import { useLocation } from 'react-router'
+import ScrollBar from 'react-smooth-scrollbar'
+
+const SmoothScrollbar = ({ children }) => {
+
+    const ref = useRef(null)
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+       const { scrollbar } = ref.current
+       scrollbar.setPosition(0,0)
+    }, [pathname])
+
+    return (
+        <ScrollBar
+            ref={ref}
+            damping='.09'
+        >
+            {
+                children
+            }
+        </ScrollBar>
+    )
+}
+
+export default SmoothScrollbar
